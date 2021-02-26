09cb8783ea44efdb5fe93f251bc4344086e4cdf8
rohanbari <rohanbari4@gmail.com>
2021-02-26 23:48:26 +0530

    Description added in operator_overload.cpp
    Defined the implementaion the rule-of-three that is widely used in C++.

--- src/026/operator_overload.cpp
+++ src/026/operator_overload.cpp
@@ -1,3 +1,21 @@
+/**
+ * @file operator_overload.cpp
+ * @author Rohan Bari (rohanbari4@gmail.com)
+ * @brief This program demonstrates an essential feature of OOP, and thus C++.
+ * It is called operator-overloading that is utilized to achieve Polymorphism
+ * to interact with objects naturally while writing expressions.
+ *
+ * @note Alongside operator-overloading, this program also describe about
+ * the implementation of Rule-of-Three that contains a copy constructor,
+ * destructor, and a copy-assignment operator overload.
+ *
+ * @version 0.1
+ * @date 2021-02-26
+ *
+ * @copyright Copyright (c) 2021 Rohan Bari
+ *
+ */
+
 #include <iostream>
 
 using std::cout;
@@ -8,7 +26,11 @@ class Math {
   int x;
 
 public:
+  // Explicit declaration for a default-constructor to prevent the possible
+  // conflicts when instantiating objects later (so they could be assigned
+  // anywhere, not necessarily at the instant where it's declared.)
   Math() {}
+  // List-initializer to initialize an object
   Math(int a) : x(a) {}
 
   // Rule of Three: Copy constructor, destructor and copy assignment operator
