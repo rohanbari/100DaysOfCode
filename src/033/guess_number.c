690a858b09b3192a5a61594e2f80d80bf1023425
rohanbari <rohanbari4@gmail.com>
2021-03-07 12:39:52 +0530

    Completed Guess the Number program

--- src/033/guess_number.c
+++ src/033/guess_number.c
@@ -6,6 +6,8 @@
  *
  * @version 0.1
  * @date 2021-03-05
+ * 
+ * @note Finished on 2021-03-07
  *
  * @copyright Copyright (c) 2021 Rohan Bari
  *
@@ -20,7 +22,9 @@ int main(void) {
   int rangeX, rangeY;
   int random_num;
   int guess_input;
+  int chances = 3;
 
+  // Seeding pseudorandomness with a unique time token
   srand(time(NULL));
 
   fprintf(stdout, "Enter the ranges <X> <Y>: ");
@@ -31,29 +35,48 @@ int main(void) {
   }
 
   do {
+    // Generating a random number between rangeX -> rangeY
     random_num = (rand() % rangeY) + rangeX;
 
-    if (fscanf(stdin, "%d", &guess_input) != 1) {
-      fprintf(stdout, "error: Input must be a valid integer.\n");
-      return EXIT_FAILURE;
-    }
+    do {
+      if (fscanf(stdin, "%d", &guess_input) != 1) {
+        fprintf(stdout, "error: Input must be a valid integer.\n");
+        return EXIT_FAILURE;
+      }
 
-    if (random_num == guess_input)
-      fprintf(stdout, "Congrats! You guessed the number.\n");
-    else if (random_num > guess_input)
-      fprintf(stdout, "hint: The number is greater than you guessed.\n");
-    else if (random_num < guess_input)
-      fprintf(stdout, "hint: The number is lesser than you guessed.\n");
-    else
-      break;
+      if (random_num == guess_input) {
+        fprintf(stdout, "Congrats! You guessed the number.\n");
+        chances = 3;
+        break;
+
+      } else if (random_num > guess_input)
+        fprintf(stdout, "hint: The number is greater than you guessed.\n");
+      else if (random_num < guess_input)
+        fprintf(stdout, "hint: The number is lesser than you guessed.\n");
+      else
+        break;
+
+      chances--;
+
+      // Discarding any possible newline
+      int c;
+      while ((c = getchar()) != '\n' && c != EOF)
+        ;
+
+    } while (random_num != guess_input && chances != 0);
+
+    if (chances == 0) {
+      fprintf(stdout, "Sorry, you are out of chances.\n");
+      chances = 3;
+    }
 
     fprintf(stdout, "Wanna try again? (Y/N) ");
 
-    if ((input = getchar()) == NULL) {
-      fprintf(stderr, "error: Input must be valid.\n");
+    if (fscanf(stdin, " %c", &input) != 1) {
+      fprintf(stdout, "error: Input must be a valid character.\n");
       return EXIT_FAILURE;
     }
-  } while (input == 'Y' && input == 'y');
+  } while (input == 'Y' || input == 'y');
 
   return EXIT_SUCCESS;
 }
