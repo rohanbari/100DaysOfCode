97a54bdc832c70f1ec06efe10c0f1de47e56aeab
rohanbari <rohanbari4@gmail.com>
2021-03-31 23:24:00 +0530

    Developed the most basic program

---
+++ src/059/simplest_code.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file main.cpp
+ * @author Rohan Bari (rohanbari4@gmail.com)
+ * @brief Meet the most basic and valid program that could be ever made in C++!
+ *
+ * @version 0.1
+ * @date 2021-03-31
+ *
+ * @copyright Copyright (c) 2021 Rohan Bari
+ *
+ */
+
+int main(void) {}
