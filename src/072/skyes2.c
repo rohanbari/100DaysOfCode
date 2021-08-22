463f88d9d74f19147fcc106c89c025339efb7ee0
rohanbari <rohanbari4@gmail.com>
2021-08-22 10:52:47 +0530

    The clock in a line program

---
+++ src/072/skyes2.c
@@ -0,0 +1,10 @@
+/*
+ * skyes2.c: It is an obfuscated version of code, popularly known as "The clock
+ * in a line program." Intuitively, it looks like this cannot be compiled, but
+ * this is quite a counter-intuitive program.
+ *
+ * @source https://stackoverflow.com/questions/15393441
+ * @date   2021-08-22
+ */
+
+main(_){_^448&&main(-~_);putchar(--_%64?32|-~7[__TIME__-_/8%8][">'txiZ^(~z?"-48]>>";;;====~$::199"[_*2&8|_/64]/(_&2?1:8)%8&1:10);}
